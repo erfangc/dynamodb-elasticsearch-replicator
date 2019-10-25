fc71043ee968647317208242191d16cfc8c16b47
Erfang Chen <erfangc@gmail.com>
2019-10-25 16:29:31 -0400

    updated the failure handling mode

--- src/main/java/com/erfangc/dynamodb/elasticsearch/Replicator.java
+++ src/main/java/com/erfangc/dynamodb/elasticsearch/Replicator.java
@@ -24,11 +24,14 @@ import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.RestStatus;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static java.lang.Integer.parseInt;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 /**
  * {@link Replicator} is a lambda class that handles DynamoDB events from a DynamoDB Stream
@@ -47,6 +50,16 @@ public class Replicator {
     private static final String USERNAME = System.getenv("ES_USERNAME");
     private static final String PASSWORD = System.getenv("ES_PASSWORD");
     private static final String INDEX = System.getenv("ES_INDEX");
+    private final RestHighLevelClient client;
+
+    public Replicator() {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(USERNAME, PASSWORD));
+        RestClientBuilder restClient = RestClient
+                .builder(new HttpHost(HOST, parseInt(PORT), SCHEME))
+                .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+        client = new RestHighLevelClient(restClient);
+    }
 
     public enum EventType {
         REMOVE, MODIFY, INSERT
@@ -58,7 +71,7 @@ public class Replicator {
      * @param event   the event object
      * @param context the context object
      */
-    public void handle(DynamodbEvent event, Context context) {
+    public void handle(DynamodbEvent event, Context context) throws IOException {
         List<DynamodbEvent.DynamodbStreamRecord> records = event.getRecords();
         BulkRequest bulkRequest = new BulkRequest();
         for (DynamodbEvent.DynamodbStreamRecord record : records) {
@@ -70,52 +83,65 @@ public class Replicator {
                 create a primary id from record
                  */
                 System.out.println(streamRecord);
-                String id = streamRecord.getKeys().entrySet().stream().map(entry -> entry.getValue().getS()).collect(joining(":"));
-
+                String id = streamRecord.getKeys().values().stream().map(AttributeValue::getS).collect(joining(":"));
                 if (EventType.valueOf(eventName) == EventType.INSERT || EventType.valueOf(eventName) == EventType.MODIFY) {
                     final Map<String, AttributeValue> newImage = streamRecord.getNewImage();
                     if (newImage == null) {
                         throw new RuntimeException("NewImage cannot be null, sequenceNumber:" + streamRecord.getSequenceNumber());
                     }
                     JsonNode payload = converter.mapToJsonObject(newImage);
-                    final IndexRequest indexRequest = new IndexRequest(INDEX, "_doc", id).source(payload.toString(), XContentType.JSON);
+                    final IndexRequest indexRequest = new IndexRequest(INDEX).id(id).source(payload.toString(), XContentType.JSON);
                     bulkRequest.add(indexRequest);
-                    System.out.println("IndexRequest:" + indexRequest.toString());
+                    System.out.println("IndexRequest: " + indexRequest.toString());
                 } else if (EventType.valueOf(eventName) == EventType.REMOVE) {
-                    final DeleteRequest deleteRequest = new DeleteRequest(INDEX, "_doc", id);
+                    final DeleteRequest deleteRequest = new DeleteRequest(INDEX).id(id);
                     bulkRequest.add(deleteRequest);
-                    System.out.println("IndexRequest:" + deleteRequest.toString());
+                    System.out.println("IndexRequest: " + deleteRequest.toString());
                 }
             } catch (JacksonConverterException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println("Sending bulk request for " + bulkRequest.requests().size() + " requests to Elasticsearch");
         executeElasticsearchRESTRequest(bulkRequest);
+        System.out.println("Executed bulk request for " + bulkRequest.requests().size() + " requests to Elasticsearch");
     }
 
-    private void executeElasticsearchRESTRequest(BulkRequest request) {
-        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(USERNAME, PASSWORD));
-        RestClientBuilder restClient = RestClient
-                .builder(new HttpHost(HOST, parseInt(PORT), SCHEME))
-                .setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
-        RestHighLevelClient client = new RestHighLevelClient(restClient);
-        try {
-            BulkResponse responses = client.bulk(request, RequestOptions.DEFAULT);
-            for (BulkItemResponse bulkItemResponse : responses.getItems()) {
-                System.out.println(bulkItemResponse.getOpType() + " id: " + bulkItemResponse.getItemId() + " status:" + bulkItemResponse.status());
-                if (bulkItemResponse.status() == RestStatus.BAD_REQUEST) {
-                    System.err.println(bulkItemResponse.getFailureMessage());
-                }
+    private void executeElasticsearchRESTRequest(BulkRequest request) throws IOException {
+        BulkResponse responses = client.bulk(request, RequestOptions.DEFAULT);
+        List<BulkItemResponse.Failure> failures = new ArrayList<>();
+        BulkItemResponse[] items = responses.getItems();
+        for (BulkItemResponse bulkItemResponse : items) {
+            System.out.println(
+                    "Response received for operation=" + bulkItemResponse.getOpType()
+                            + " index=" + bulkItemResponse.getIndex()
+                            + " itemId=" + bulkItemResponse.getItemId()
+                            + " status=" + bulkItemResponse.status()
+            );
+            if (bulkItemResponse.status() == RestStatus.BAD_REQUEST) {
+                failures.add(bulkItemResponse.getFailure());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                client.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        }
+        if (!failures.isEmpty()) {
+            String message = failures.size() + " out of " + items.length + " requests to Elasticsearch failed";
+            List<IndexFailure> indexFailures = failures
+                    .stream()
+                    .map(
+                            failure -> new IndexFailure()
+                                    .setCause(failure.getCause().getMessage())
+                                    .setId(failure.getId())
+                                    .setIndex(failure.getIndex())
+                                    .setTimestamp(Instant.now().toString())
+                    )
+                    .collect(toList());
+            indexFailures.forEach(
+                    failure -> System.err.println(
+                            "Failure detail: id=" + failure.getId()
+                                    + " message=" + failure.getCause()
+                                    + " index=" + failure.getIndex()
+                    )
+            );
+            throw new RuntimeException(message);
         }
     }
 
